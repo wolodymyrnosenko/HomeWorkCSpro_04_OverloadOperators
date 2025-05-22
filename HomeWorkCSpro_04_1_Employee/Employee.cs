5e63ce532c137cb6637c041c986a66233abd401f
volodymyr <vovanose@ukr.net>
2025-05-22 13:23:09 +0300

    Add task 04_1 overload Employee

---
+++ HomeWorkCSpro_04_1_Employee/Employee.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCSpro_04_1_Employee
+{
+    internal class Employee
+    {
+        private string name;
+        private string position;
+        private int salary;
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public string Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public int Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value >= 0)
+                    salary = value;
+                else
+                    throw new ArgumentException("Зарплата не може бути від'ємною.");
+            }
+        }
+
+        public Employee(string name, string position, int salary)
+        {
+            Name = name;
+            Position = position;
+            Salary = salary;
+        }
+
+        public static Employee operator +(Employee employee, int sum)
+        {
+            return new Employee(employee.Name, employee.Position, employee.Salary + sum);
+        }
+
+        public static Employee operator -(Employee employee, int sum)
+        {
+            return new Employee(employee.Name, employee.Position, employee.Salary - sum);
+        }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.Salary == employee2.Salary;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Salary != employee2.Salary;
+        }
+
+        public static bool operator <(Employee employee1, Employee employee2)
+        {
+            return employee1.Salary < employee2.Salary;
+        }
+
+        public static bool operator >(Employee employee1, Employee employee2)
+        {
+            return employee1.Salary > employee2.Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Ім'я: {Name} \tПосада: {Position} \tЗарплата: {Salary} грн";
+        }
+    }
+}
---
+++ HomeWorkCSpro_04_1_Employee/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace HomeWorkCSpro_04_1_Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Employee emp1 = new Employee("Петро", "Інженер", 20000);
+            Employee emp2 = new Employee("Олена", "Бухгалтер", 35000);
+
+            emp1 = emp1 + 5000;
+            emp2 = emp2 - 2000;
+
+            Console.WriteLine(emp1);
+            Console.WriteLine(emp2);
+
+            Console.WriteLine(emp1 == emp2);
+            Console.WriteLine(emp1 > emp2);
+            Console.WriteLine(emp1 < emp2);
+        }
+    }
+}
