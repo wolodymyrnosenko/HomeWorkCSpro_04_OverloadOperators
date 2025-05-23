7e24e911e9143b7d23d95fc60e8915e93e983f66
volodymyr <vovanose@ukr.net>
2025-05-23 15:29:59 +0300

    Add task 4_4 Class Matrix

---
+++ HomeWorkCSpro_04_4_Matrix/Matrix.cs
@@ -0,0 +1,174 @@
+﻿using System;
+
+namespace HomeWorkCSpro_04_4_Matrix
+{
+    internal class Matrix
+    {
+        private int _rows;
+        private int _columns;
+        public int Rows
+        {
+            get { return _rows; }
+            private set
+            {
+                if (value > 0)
+                    _rows = value;
+            }
+        }
+        public int Columns
+        {
+            get { return _columns; }
+            private set
+            {
+                if (value > 0)
+                    _columns = value;
+            }
+        }
+        public double[][] Numbers { get; private set; }
+        public Matrix(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            Numbers = new double[Rows][];
+            for(int i = 0; i < Rows; i++)
+                Numbers[i] = new double[Columns];
+        }
+        //Indexator [,]
+        public double this[int i, int j]
+        {
+            get
+            {
+                return Numbers[i][j];
+            }
+            set
+            {
+                Numbers[i][j] = value;
+            }
+        }
+
+        //Indexator [][]
+        public RowClass this[int i]
+        {
+            get { return new RowClass(this, i); }
+        }
+
+    public class RowClass
+        {
+            private readonly Matrix matrixRowClass;
+            private readonly int rowIndex;
+            internal RowClass(Matrix matrixRowClass, int rowIndex)
+            {
+                this.matrixRowClass = matrixRowClass;
+                this.rowIndex = rowIndex;
+            }
+        
+            public double this[int j]
+            {
+                get
+                {
+                    return matrixRowClass.Numbers[rowIndex][j];
+                }
+                set
+                {
+                    matrixRowClass.Numbers[rowIndex][j] = value;
+                }
+            }
+        }
+
+        public void Init()
+        {
+            Random random = new Random();
+            for (int i = 0; i < Rows; i++) 
+                for (int j = 0; j < Columns; j++)
+                    //Numbers[i][j] = random.Next(1, 10);
+                    //this[i,j] = random.Next(1, 10);
+                    this[i][j] = random.Next(1, 10);
+        }
+
+        public void ConsoleOutPut()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    //Console.Write($"{Numbers[i][j]}\t");
+                    Console.Write($"{this[i, j]}\t");
+                    //Console.Write($"{this[i][j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static bool IsValidToOperate(Matrix matrix1, Matrix matrix2)
+        {
+            return matrix1.Rows == matrix2.Rows && matrix1.Columns == matrix2.Columns;
+        }
+
+        public static Matrix operator +(Matrix matrix1, Matrix matrix2)
+        {
+            if (!IsValidToOperate(matrix1, matrix2))
+                throw new ArgumentException("Matrixes are not valid");
+            Matrix result = new Matrix(matrix1.Rows, matrix1.Columns);
+            for (int i = 0; i < matrix2.Rows; i++)
+                for (int j = 0; j < matrix2.Columns; j++)
+                    result[i][j] = matrix1[i][j] + matrix2[i][j];
+            return result;
+        }
+
+        public static Matrix operator -(Matrix matrix1, Matrix matrix2)
+        {
+            if (!IsValidToOperate(matrix1, matrix2))
+                throw new ArgumentException("Matrixes are not valid");
+            Matrix result = new Matrix(matrix1.Rows, matrix1.Columns);
+            for (int i = 0; i < matrix2.Rows; i++)
+                for (int j = 0; j < matrix2.Columns; j++)
+                    result[i][j] = matrix1[i][j] - matrix2[i][j];
+            return result;
+        }
+
+        public static Matrix operator *(Matrix matrix1, Matrix matrix2)
+        {
+            if (!IsValidToOperate(matrix1, matrix2))
+                throw new ArgumentException("Matrixes are not valid");
+            Matrix result = new Matrix(matrix1.Rows, matrix1.Columns);
+            for (int i = 0; i < matrix2.Rows; i++)
+                for (int j = 0; j < matrix2.Columns; j++)
+                    result[i][j] = matrix1[i][j] * matrix2[i][j];
+            return result;
+        }
+        public static Matrix operator *(Matrix matrix1, double number)
+        {
+            Matrix result = new Matrix(matrix1.Rows, matrix1.Columns);
+            for (int i = 0; i < matrix1.Rows; i++)
+                for (int j = 0; j < matrix1.Columns; j++)
+                    result[i][j] = matrix1[i][j] * number;
+            return result;
+        }
+        public static bool operator ==(Matrix matrix1, Matrix matrix2)
+        {
+            if (!IsValidToOperate(matrix1, matrix2))
+                throw new ArgumentException("Matrixes are not valid");
+            for (int i = 0; i < matrix1.Rows; i++)
+                for (int j = 0; j < matrix1.Columns; j++)
+                    if (matrix1[i][j] != matrix2[i][j])
+                        return false;
+            return true;
+        }
+        public static bool operator !=(Matrix matrix1, Matrix matrix2)
+        {
+            return !(matrix1 == matrix2);
+        }
+
+        public override string ToString()
+        {
+            string str = string.Empty;
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                    str += $"{this[i][j]}\t";
+                str += Environment.NewLine;
+            }
+            return str;
+        }
+    }
+}
---
+++ HomeWorkCSpro_04_4_Matrix/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace HomeWorkCSpro_04_4_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix1 = new Matrix(3, 5);
+            matrix1.Init();
+            matrix1.ConsoleOutPut();
+            Console.WriteLine("----------------------------------");
+            Matrix matrix2 = new Matrix(3, 5);
+            matrix2.Init();
+            matrix2.ConsoleOutPut();
+            Console.WriteLine("----------------------------------");
+            matrix2.Numbers[1][1] = 55;//Although private set there isn't exception, why?
+            matrix2[1][2] = 77;
+            matrix2.ConsoleOutPut();
+            Console.WriteLine("----------------------------------");
+            var addMatrix = matrix1 + matrix2;
+            addMatrix.ConsoleOutPut();
+            Console.WriteLine("----------------------------------");
+            var subMatrix = matrix1 - matrix2;
+            subMatrix.ConsoleOutPut();
+            Console.WriteLine("----------------------------------");
+            var multMatrix = matrix1 * matrix2;
+            multMatrix.ConsoleOutPut();
+            Console.WriteLine("----------------------------------");
+            var multNumbetMatrix = matrix1 * 10;
+            multNumbetMatrix.ConsoleOutPut();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine($"Matrixes are equals - {matrix1 == matrix2}");
+            Console.WriteLine($"Matrixes are not equals - {matrix1 != matrix2}");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine(matrix1 + matrix2);
+            Console.WriteLine(matrix1);
+            Console.WriteLine("----------------------------------");
+            Matrix matrix3 = new Matrix(1, 7);
+            matrix3.Init();
+            try
+            {
+                Console.WriteLine(matrix3 + matrix2);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine(matrix3);
+        }
+    }
+}
